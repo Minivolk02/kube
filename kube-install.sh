0913de09ab3d277de70c8b3cbe1213525ee3e66f
Yaroslav Volkov <minivolk02@gmail.com>
2022-10-26 17:05:52 +0300

    Add files via upload

---
+++ kube-install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+apt install sudo curl net-tools -y
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+sudo swapoff -a
+
+curl -s -o - https://get.docker.com | bash -
