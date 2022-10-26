2395fee3dd636fcf1ea315f4fc1a189dacff99eb
Yaroslav Volkov <minivolk02@gmail.com>
2022-10-26 17:10:36 +0300

    Update kube-install.sh

--- kube-install.sh
+++ kube-install.sh
@@ -21,3 +21,8 @@ sudo sysctl --system
 sudo swapoff -a
 
 curl -s -o - https://get.docker.com | bash -
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt update -y
+sudo apt-get install -y kubelet=1.19.6-00 kubeadm=1.19.6-00 kubectl=1.19.6-00
